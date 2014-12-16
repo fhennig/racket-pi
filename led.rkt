2d38fcd1150034689248361a24b592c945897a0f
Felix Hennig <github@felixhennig.com>
2014-12-16 19:03:18 +0100

    stuff

--- led.rkt
+++ led.rkt
@@ -1,18 +1,12 @@
 #lang racket
 
-(require "c-interface.rkt")
+(require "wiringPi-wrapper.rkt")
+(provide (all-defined-out))
 
 (define CYCLE-LENGTH 100)
 
+; an rgb-color. 0 <= value <= 1
 (define-struct rgb (r g b))
-
-(define WHITE (make-rgb 1 1 1))
-(define BLACK (make-rgb 0 0 0))
-(define RED (make-rgb 1 0 0))
-(define YELLOW (make-rgb 1 1 0))
-(define GREEN (make-rgb 0 1 0))
-(define BLUE (make-rgb 0 0 1))
-(define PURPLE (make-rgb 1 0 1))
 
 (define-struct strip (r-pin g-pin b-pin))
 
@@ -22,11 +16,6 @@
   (softPwmCreate b-pin 0 CYCLE-LENGTH)
   (make-strip r-pin g-pin b-pin))
 
-(define s1 (make-strip/init 6 10 11))
-(define s2 (make-strip/init 0 2 3))
-(define s3 (make-strip/init 8 9 7))
-(define s4 (make-strip/init 12 13 14))
-
 (define (set-color strip rgb)
   (let ([int-val (lambda (frac)
 		   (inexact->exact (floor (* frac CYCLE-LENGTH))))])
@@ -35,8 +24,3 @@
     (softPwmWrite (strip-b-pin strip) (int-val (rgb-b rgb)))))
 
 (void (wiringPiSetup))
-
-(set-color s1 WHITE)
-(set-color s2 WHITE)
-(set-color s3 WHITE)
-(set-color s4 WHITE)
---
+++ server.rkt
@@ -0,0 +1,46 @@
+#lang racket
+
+(require "led.rkt")
+(require web-server/dispatch
+	 web-server/servlet
+	 web-server/servlet-env)
+(provide/contract (my-dispatch (request? . -> . response?)))
+
+;; mapping pins to strips
+(define strips
+  (list 
+   (make-strip/init 6 10 11)
+   (make-strip/init 0 2 3)))
+
+
+(define-values (my-dispatch my-url)
+  (dispatch-rules
+   [("set" (integer-arg) (number-arg) (number-arg) (number-arg)) set-handler]
+   [else error-handler]))
+
+(define (strip-id-valid? s)
+  (and (>= s 0) (< s (length strips))))
+
+(define (color-arg-valid? a)
+  (and (>= a 0) (<= a 1)))
+
+(define (set-handler req strip-id r g b)
+  (let ([strip-val? (lambda (s) (and (>= s 0) (< s (length strips))))]
+	[color-val? (lambda (a) (and (>= a 0) (<= a 1)))])
+    (cond [(and (strip-val? strip-id) (color-val? r)
+		(color-val? g) (color-val? b))
+	   (set-color (list-ref strips strip-id)
+		      (make-rgb r g b))
+	   (response/xexpr "ok")]
+	  [else (error-handler 'nothing)])))
+
+(define (error-handler req)
+  (response/xexpr "error"))
+
+(serve/servlet my-dispatch
+               #:launch-browser? #f
+               #:quit? #f
+               #:listen-ip #f
+               #:port 80
+               #:servlet-path ""
+	       #:servlet-regexp #rx"")
