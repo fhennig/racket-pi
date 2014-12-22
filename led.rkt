76b0dfdf4243478ea7f065a6297bac0fe8987eae
Felix Hennig <github@felixhennig.com>
2014-12-22 18:13:10 +0100

    Added internal Hashmap to aquire led-color later on

--- led.rkt
+++ led.rkt
@@ -1,26 +1,73 @@
 #lang racket
 
 (require "wiringPi-wrapper.rkt")
-(provide (all-defined-out))
+(provide (struct-out led)
+	 (struct-out color)
+	 valid-color
+	 set-led-color!
+	 led-color
+	 initialize)
+
+
 
 (define CYCLE-LENGTH 100)
 
 ; an rgb-color. 0 <= value <= 1
-(define-struct rgb (r g b))
+(struct color (r g b))
 
-(define-struct strip (r-pin g-pin b-pin))
+(define (valid-color r g b)
+  (if (and (and (>= r 0) (<= r 1))
+	   (and (>= g 0) (<= g 1))
+	   (and (>= b 0) (<= b 1)))
+      (color r g b)
+      #f))
 
-(define (make-strip/init r-pin g-pin b-pin)
-  (softPwmCreate r-pin 0 CYCLE-LENGTH)
-  (softPwmCreate g-pin 0 CYCLE-LENGTH)
-  (softPwmCreate b-pin 0 CYCLE-LENGTH)
-  (make-strip r-pin g-pin b-pin))
 
-(define (set-color strip rgb)
+(struct led (name r-pin g-pin b-pin))
+
+(define (init led [color (color 0 0 0)])
+  (softPwmCreate (led-r-pin led) (color-r color) CYCLE-LENGTH)
+  (softPwmCreate (led-g-pin led) (color-g color) CYCLE-LENGTH)
+  (softPwmCreate (led-b-pin led) (color-b color) CYCLE-LENGTH)
+  (hash-set! color-dict led color))
+
+(define (initialize leds)
+  (wiringPiSetup)
+  (map init leds)
+  (void))
+
+(define color-dict (make-hash))
+
+(define (led-color led)
+  (hash-ref color-dict led))
+
+(define (set-led-color! led color)
+  (set-color (led-r-pin led)
+	     (led-g-pin led)
+	     (led-b-pin led) color)
+  (hash-set! color-dict led color))
+
+; sets color on the pins r g b
+(define (set-color r g b color)
   (let ([int-val (lambda (frac)
 		   (inexact->exact (floor (* frac CYCLE-LENGTH))))])
-    (softPwmWrite (strip-r-pin strip) (int-val (rgb-r rgb)))
-    (softPwmWrite (strip-g-pin strip) (int-val (rgb-g rgb)))
-    (softPwmWrite (strip-b-pin strip) (int-val (rgb-b rgb)))))
+    (softPwmWrite r (int-val (color-r color)))
+    (softPwmWrite g (int-val (color-g color)))
+    (softPwmWrite b (int-val (color-b color)))))
 
-(void (wiringPiSetup))
+;; Debug functions
+
+(define (led->string led)
+  (string-append "LED " (led-name led)))
+
+(define (color->string c)
+  (string-append "COLOR R=" (number->string (color-r c))
+		 " G=" (number->string (color-g c))
+		 " B=" (number->string (color-b c))))
+
+(define led1 (led "LED1" 1 2 3))
+(define led2 (led "LED2" 4 5 6))
+
+(initialize (list led1 led2))
+
+(set-led-color! led1 (color 1 1 1))
--- server.rkt
+++ server.rkt
@@ -7,12 +7,11 @@
 (provide/contract (my-dispatch (request? . -> . response?)))
 
 ;; mapping pins to strips
-(define strips
-  (list 
-   (make-strip/init 15 16 1) ; 0
-   (make-strip/init 0 2 3)   ; 1
-   (make-strip/init 8 9 7)   ; 2
-   (make-strip/init 12 13 14)))
+(define leds
+  (list (led "oben rechts" 15 16 1)
+	(led "unten rechts" 0 2 3)  
+	(led "oben links" 8 9 7)  
+	(led "unten links" 12 13 14)))
 
 (define-values (my-dispatch my-url)
   (dispatch-rules
@@ -24,31 +23,30 @@
 
 ;; semi-predicates for validation
 
-(define (percent-values->rgb r g b)
-  (if (and (and (>= r 0) (<= r 1))
-	   (and (>= g 0) (<= g 1))
-	   (and (>= b 0) (<= b 1)))
-      (make-rgb r g b)
-      #f))
-
 (define (strip-id->strip-setter id)
-  (cond [(and (>= id 0) (< id (length strips)))
-	 (curry set-color (list-ref strips id))]
+  (cond [(and (>= id 0) (< id (length leds)))
+	 (curry set-led-color! (list-ref leds id))]
 	[(< id 0) (lambda (color)
-		    (map (curryr set-color color) strips))]
+		    (map (curryr set-led-color! color) leds))]
 	[else #f]))
 
 ;; handlers
 
 (define (set-perc-handler req strip-id r g b)
   (let ([strip-setter (strip-id->strip-setter strip-id)]
-	[color (percent-values->rgb r g b)])
+	[color (valid-color r g b)])
     (set/respond strip-setter color)))
 
 (define (set/respond strip-setter color)
-  (cond [(and strip color) (strip-setter color)
+  (cond [(and strip-setter color) (strip-setter color)
 	 ok-response]
 	 [else error-response]))
+
+
+
+;; Starting the application
+
+(initialize leds)
 
 (serve/servlet my-dispatch
                #:launch-browser? #f
